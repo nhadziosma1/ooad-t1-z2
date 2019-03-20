82c0ec3cab171058ca17507a9eca892b500d6450
nhadziosma1 <nhadziosma1@etf.unsa.ba>
2019-03-20 13:56:18 +0100

    -metoda "Write" ne pravi "\n" na kraju -u metodi "WriteLine" automatki se na kraju ododaje "\n"

--- ooad-t1-z2/ooad-t1-z2/Program.cs
+++ ooad-t1-z2/ooad-t1-z2/Program.cs
@@ -6,7 +6,27 @@ namespace ooad_t1_z2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Unesite cijeli broj koji oznacava broj spratova trougla/piramide: ");
+            int brojNivoa;
+            while (Int32.TryParse(Console.ReadLine(), out brojNivoa) == false)
+                Console.WriteLine("pogrešan unos, unesite ponovno: ");
+
+            for(int i=0; i<brojNivoa; i++)
+            {
+                for(int j=0; j<2*brojNivoa-1; j++)
+                {
+                    if (j < brojNivoa && i+j>=brojNivoa-1)
+                        Console.Write("*");
+                    else if(j>=brojNivoa && j<2*brojNivoa-1 && j-i<brojNivoa)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
+        
     }
 }
