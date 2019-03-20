22fad0b3adeae296890f8eed1955e7ee2300a6ac
nhadziosma1 <nhadziosma1@etf.unsa.ba>
2019-03-20 13:44:02 +0100

    prvi komit zadatka 2

---
+++ ooad-t1-z2/ooad-t1-z2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ooad_t1_z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
